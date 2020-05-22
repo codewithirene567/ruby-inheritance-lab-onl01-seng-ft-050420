d8082f2d15bd183ca2c734999f2f9f34d5e43355
Irene Scott <irenejinscott@gmail.com>
2020-05-22 15:09:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User 
+  def initialize
+   @knowledge = []
+  end
+
+def learn("")
+  @knowledge << ("")
+end
+
+def knowledge
+  @knowledge
+end
 
 end
+#teachers and students will be inheriting from the user model
